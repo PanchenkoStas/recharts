3461fa0878dd3615799dd20b6e4ea33bb8c21ed2
test <test>
2021-06-14 10:23:05 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  Line,
+  LineChart,
+} from 'recharts';
 
-function App() {
+const data = [
+  {
+    date: '28-Feb',
+    monthly: 700,
+    weekly: 1200,
+    daily: 500,
+  },
+  {
+    date: '7-Mar',
+    monthly: 800,
+    weekly: 1300,
+    daily: 650,
+  },
+  {
+    date: '14-Mar',
+    monthly: 850,
+    weekly: 1150,
+    daily: 500,
+  },
+  {
+    date: '21-Mar',
+    monthly: 650,
+    weekly: 1200,
+    daily: 550,
+  },
+  {
+    date: '28-Mar',
+    monthly: 800,
+    weekly: 1300,
+    daily: 600,
+  },
+];
+
+function ActiveInfo() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="active-info">
+      <h1>Active Community Members</h1>
+      <div className="active-info__monthly">
+        <span>Monthly</span>
+        <div className="active-members">1.2m</div>
+      </div>
+      <div className="active-info__weekly">
+        <span>Weekly</span>
+        <div className="active-members">830k</div>
+      </div>
+      <div className="active-info__daily">
+        <span>Daily</span>
+        <div className="active-members">485k</div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active) {
+      return (
+        <div className="tooltip">
+          <h4>Activity</h4>
+          <p className="subtitle">+22,5%</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className="container">
+      <ActiveInfo />
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart margin={{ left: 30, top: 20, right: 30 }} data={data}>
+          <Line
+            dataKey="monthly"
+            type="monotone"
+            stroke="blue"
+            strokeWidth={2}
+            activeDot={{ r: 7 }}
+          />
+          <Line
+            dataKey="weekly"
+            type="monotone"
+            stroke="blueviolet"
+            strokeWidth={2}
+            activeDot={{ r: 7 }}
+          />
+          <Line
+            dataKey="daily"
+            type="monotone"
+            stroke="violet"
+            strokeWidth={2}
+            activeDot={{ r: 7 }}
+          />
+          <XAxis dataKey="date" tickLine={false} />
+          <YAxis datakey="value" tickLine={false} tickCount={6} />
+          <Tooltip content={<CustomTooltip />} />
+          <CartesianGrid vertical={true} stroke="#f5f5f5" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
